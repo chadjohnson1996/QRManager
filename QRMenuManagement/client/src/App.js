f05de3da6df10a600291118ddfda374ea712e77d
Chad Johnson <chad.johnson.1996@gmail.com>
2020-10-13 05:18:21 -0700

    Set up example routing

--- QRMenuManagement/client/src/App.js
+++ QRMenuManagement/client/src/App.js
@@ -1,58 +1,38 @@
 import React from 'react';
-import { Fetch } from 'react-request';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import { DummyApp } from './features/dummy_app/DummyApp';
+import { QrManager } from './features/qr_manager/QrManager';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dummy App</Link>
+            </li>
+            <li>
+              <Link to="/QRManager">QR Manager</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/QRManager">
+            <QrManager />
+          </Route>
+          <Route path="/">
+            <DummyApp />
+          </Route>
+        </Switch>
+      </div>  
+    </Router>
   );
 }
 
